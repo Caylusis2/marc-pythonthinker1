6fcf998f28ada089e6f6de114b85807be3eeaf46
Caylusis2 <caylusis2@yahoo.com>
2025-01-25 06:23:42 +0000

    Jan 25, 2025, 6:23 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -3,7 +3,7 @@ print("Hello from lesson 3")
 # create variable (adrian)
 # create varialbe (brandon)
 # create variable (daniel)
-# create variable ()
+# create variable (charlie)
 # create variable (average score)
 
 # 
