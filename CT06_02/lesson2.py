6e97a79b81a12f84bc6f3f31c5de91d3ad656bd4
Caylusis2 <caylusis2@yahoo.com>
2025-01-25 05:52:03 +0000

    Jan 25, 2025, 5:52 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,3 +5,40 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+
+print("red")
+print("yellow")
+print("orange")
+print("green")
+print("blue")
+print("violet")
+print("purple")
+
+# repeat ten times
+# say hey!
+# move ten steps
+
+# assign 0 to "counter"
+# while until "counter" is 50:
+# move ten steps
+# turn 15 degrees
+# change counter by 10
+
+# ask user what is their age and wait
+# if their answer is smaller than 18 then say access denied 
+# else say welcome if answer is not smaller than 18
+
+# create a variable password
+# Ask the user to imput the password
+# if answer = password variable
+# print("Congratulations")
+# else
+#     print("Access denied")
+
+# create a variable first test 
+# create a variable second test
+# create a variable third test
+# create a variable
+
+
+
