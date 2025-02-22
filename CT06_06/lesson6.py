0b83a3264544f0411cf803841b442d5897859364
Caylusis2 <caylusis2@yahoo.com>
2025-02-22 07:12:38 +0000

    Feb 22, 2025, 7:12 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,9 +1,26 @@
-print("Hello from lesson 6")
+# print("Hello from lesson 6")
 
-num_of_students=int(input("How many students are there?"))
-score_of_students=0
-for i in range(num_of_students):
-    score_of_students= score_of_students + int(input("What is the students score"))
+# num_of_students=int(input("How many students are there?"))
+# score_of_students=0
+# for i in range(num_of_students):
+#     score_of_students= score_of_students + int(input("What is the students score"))
 
-average_score= score_of_students/num_of_students
-print(average_score)
+# average_score= score_of_students/num_of_students
+# print(average_score)
+
+# **Task 1a**:
+for i in range(3):
+    print("Hello, World!")
+
+# **Task 1b**:
+for i in range(5):
+    print(i)
+
+# **Task 1c**:
+print("Hello, World!")
+
+# **Task 1d**:
+i = 5
+
+# **Task 1e**:
+print("Hello, World!")
