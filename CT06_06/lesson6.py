e3b44896510c415d0380592cda13113cee8b232d
Caylusis2 <caylusis2@yahoo.com>
2025-02-22 06:56:35 +0000

    Feb 22, 2025, 6:56 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -2,4 +2,4 @@ print("Hello from lesson 6")
 
 num_of_students=input("How many students are there?")
 for i in range(num_of_students):
-    score_of_students=scoreinput("What is the students score")
+    score_of_students=score_of_students+input("What is the students score")
