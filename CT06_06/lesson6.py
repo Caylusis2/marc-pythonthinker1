001270f75e7bea0fb2aaa4b57dae53d72f1eb65a
Caylusis2 <caylusis2@yahoo.com>
2025-02-22 07:00:55 +0000

    Feb 22, 2025, 7:00 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -5,5 +5,5 @@ score_of_students=0
 for i in range(num_of_students):
     score_of_students= score_of_students + int(input("What is the students score"))
 
-average_score= score
+average_score= score_of_students/
 print()
