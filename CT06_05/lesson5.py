137f665f87ff592617f3fe2f85a747cbf19d3b0d
Caylusis2 <caylusis2@yahoo.com>
2025-02-22 06:51:45 +0000

    Feb 22, 2025, 6:51 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,27 @@
-print("Hello from lesson 5")
+# print("Hello from lesson 5")
+
+# for i in range(2, 26, 2):
+#     print(i)
+
+# for i in range(8, 104, 8):
+#     print(i)
+
+# for i in range(5, 0, -1):
+#     print(i)
+
+# for i in range(3, 0, -1):
+#     print("Ready!")
+
+start= int(input("Give me a number"))
+end= input("Give me a number")
+
+if start > end:
+    for i in range(start, end-1, -1):
+        print(i)
+else: 
+    for i in range(end, start-1, -1):
+        print(i)
+
+
+
+
--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,3 @@
 print("Hello from lesson 6")
+
+num_of_students=input("How many students are there?")
