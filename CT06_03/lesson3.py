58793f55577a2fd746e5effface74fcbeb6da27d
Caylusis2 <caylusis2@yahoo.com>
2025-03-01 06:07:47 +0000

    Mar 1, 2025, 6:07 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -17,50 +17,50 @@
 # variable (total score)
 
 
-age= 12
-print(age)
+# age= 12
+# print(age)
 
 
-x= 10+20
-print(x)
+# x= 10+20
+# print(x)
 
-var1= 20
-var2= 5
-sum= var1+var2
-print(sum)
+# var1= 20
+# var2= 5
+# sum= var1+var2
+# print(sum)
 
-diff= var1-var2
-print(diff)
+# diff= var1-var2
+# print(diff)
 
-prod=var1*var2
-print(prod)
+# prod=var1*var2
+# print(prod)
 
-div=var1/var2
-print(div)
+# div=var1/var2
+# print(div)
 
-a=86
-b=91
-sum=a+b
-print(sum)
+# a=86
+# b=91
+# sum=a+b
+# print(sum)
 
-num1=10
-num2=50
-num3=3
+# num1=10
+# num2=50
+# num3=3
 
-subtraction=num2-num1
-ans=subtraction*num3
-print(ans)
+# subtraction=num2-num1
+# ans=subtraction*num3
+# print(ans)
 
-num1=100
-num2=4
-division=num1/num2
-print(division)
+# num1=100
+# num2=4
+# division=num1/num2
+# print(division)
 
-a= 3+4
-b= 10/2
-c= a*5
-d= c-b
-print(d)
+# a= 3+4
+# b= 10/2
+# c= a*5
+# d= c-b
+# print(d)
 
 
 
