657c09e1c0afde90ea46e5b4d3b475e48e5481c4
Caylusis2 <caylusis2@yahoo.com>
2025-03-01 06:38:19 +0000

    Mar 1, 2025, 6:38 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -13,8 +13,8 @@
 # print("Average score for " + student_name + " is: " + str(average_score))
 
 
-for i in range(1 , 11, 1):
-    print(i)
+# for i in range(1 , 11, 1):
+#     print(i)
 
 for i in range(2 , 20, 2):
     print(i)
