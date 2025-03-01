32ff75d6a11aa997582c8dee47ffa74fe1ddf161
Caylusis2 <caylusis2@yahoo.com>
2025-03-01 07:17:32 +0000

    Mar 1, 2025, 7:17 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -39,5 +39,5 @@ sum=0
 for i in range(1, 6):
     sum = sum + int(input("What is the number #" + str(i)))
 
-print("Sum of the five number is" + sum)
+print("Sum of the five number is" + str(sum))
 
