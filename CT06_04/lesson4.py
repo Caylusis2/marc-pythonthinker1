3cdc31cde8229b077c738227ca13f123a9e35f8a
Caylusis2 <caylusis2@yahoo.com>
2025-03-01 06:07:20 +0000

    Mar 1, 2025, 6:07 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,33 +1,33 @@
-print("Hello from lesson 4")
+# print("Hello from lesson 4")
 
-red_price= 1
-blue_price= 2
-green_price= 3
+# red_price= 1
+# blue_price= 2
+# green_price= 3
 
-red_plates= 3
-blue_plates= 5
-green_plates= 4
+# red_plates= 3
+# blue_plates= 5
+# green_plates= 4
 
-red_total=red_plates*red_price
-blue_total=blue_plates*blue_price
-green_total=green_plates*green_price
-total=red_total+blue_total+green_total
-print(total)
+# red_total=red_plates*red_price
+# blue_total=blue_plates*blue_price
+# green_total=green_plates*green_price
+# total=red_total+blue_total+green_total
+# print(total)
 
-name=input("What is your name?")
-print("Hi, "+ name)
+# name=input("What is your name?")
+# print("Hi, "+ name)
 
-colour=input("What is your favourite colour?")
-print(colour)
+# colour=input("What is your favourite colour?")
+# print(colour)
 
-age=input("What is your age?")
-print(age)
+# age=input("What is your age?")
+# print(age)
 
-hobby=input("What is your favourite hobby?")
-print("I enjoy "+ hobby)
+# hobby=input("What is your favourite hobby?")
+# print("I enjoy "+ hobby)
 
-dv=input("Where is your dream vacation?")
-print("I would love to visit "+ dv)
+# dv=input("Where is your dream vacation?")
+# print("I would love to visit "+ dv)
 
 
 
