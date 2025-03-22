49af6fe47f50ad5948c5021644eb3dfb1537e05c
Caylusis2 <caylusis2@yahoo.com>
2025-03-22 07:30:38 +0000

    Mar 22, 2025, 7:30 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -39,7 +39,7 @@ num_apples=input("How many apples do you wan tot buy?")
 num_oranges=input("How many oranges do you want to buy?")
 
 if num_apples > 5:
-    costApple
+    costApple = num_apple
 
 
 
