12430604a40c9ac0b3ad4ce93c2e912497fa918b
Caylusis2 <caylusis2@yahoo.com>
2025-03-22 07:08:46 +0000

    Mar 22, 2025, 7:08 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -18,7 +18,7 @@ print("Hello from lesson 9")
 
 # print(all_even_add)
 
-numberofdays=input("How many days have your book been borrowed?")
+numberofdays=int(input("How many days have your book been borrowed?"))
 
 if numberofdays > 25:
     print("Remember to return your book!")
