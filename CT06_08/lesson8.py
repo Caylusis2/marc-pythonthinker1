5568ff8736a4f0f5b71caf848acbcefd840302a0
Caylusis2 <caylusis2@yahoo.com>
2025-03-22 07:05:42 +0000

    Mar 22, 2025, 7:05 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -66,23 +66,7 @@
 #     useranswer=int(input("What is " + str(num1) + " x " + str(num2) + "?"))
 #     print(num1 * num2 == useranswer)
 
-import random
 
-num1= random.randint(1, 6)
-num2= random.randint(1, 6)
-num3= random.randint(1, 6)
-
-print("1st number is: " + str(num1))
-print("2nd number is: " + str(num2))
-print("3rd number is: " + str(num3))
-
-num1_isEven= num1 % 2 == 0
-num2_isEven= num1 % 2 == 0
-num3_isEven= num3 % 2 == 0
-
-all_even_add = num1_isEven == num2_isEven
-
-print(all_even_add)
 
 
 
