7fea85950350ae3d951365c0b016bc0dae7368c9
Caylusis2 <caylusis2@yahoo.com>
2025-03-29 06:39:24 +0000

    Mar 29, 2025, 6:39 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -3,7 +3,7 @@ print("Hello from lesson 10")
 import random
 
 random.randint(1, 15)
-guess=input("Guess a number")
+guess=input("Guess a number from 1 to 15")
 
 if random.randint == guess:
     print("That's it!")
