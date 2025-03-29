aac4b9b04683e22c0a84f0170dfd38adedacf2a4
Caylusis2 <caylusis2@yahoo.com>
2025-03-29 06:53:06 +0000

    Mar 29, 2025, 6:53 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,7 +1,9 @@
-print("Hello from lesson 10")
+# print("Hello from lesson 10")
 
-int=int(input("Give me a number"))
-if int < 0:
-    print("Negative")
-else:
-    print("Positive")
+# int=int(input("Give me a number"))
+# if int < 0:
+#     print("Negative")
+# else:
+#     print("Positive")
+
+
