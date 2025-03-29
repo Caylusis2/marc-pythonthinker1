03de1adcfe00b605f5d367a448fb19225d6e7987
Caylusis2 <caylusis2@yahoo.com>
2025-03-29 07:19:28 +0000

    Mar 29, 2025, 7:19 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -13,26 +13,30 @@
 # else:
 #     print("This number is odd")
 
-# age=int(input("What is your age?"))
+age=int(input("What is your age?"))
 
-# if age < 13:
-#     print("Child")
-# elif age < 19:
+if age < 13:
+    print("Child")
+elif age < 19:
+    p
+
+
+
     
         
         
-score=int(input("What is your score?"))
+# score=int(input("What is your score?"))
 
-if score > 90:
-    print("A")
-elif score > 80:
-    print("B")
-elif score > 70:
-    print("C")
-elif score > 60:
-    print("D")
-else:
-    print("F")
+# if score > 90:
+#     print("A")
+# elif score > 80:
+#     print("B")
+# elif score > 70:
+#     print("C")
+# elif score > 60:
+#     print("D")
+# else:
+#     print("F")
 
 
 
