842ad69eada9fd9c9a50f49d9355556fb885734a
Caylusis2 <caylusis2@yahoo.com>
2025-04-19 06:42:09 +0000

    Apr 19, 2025, 6:42 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,34 +1,36 @@
 # print("Hello from lesson 13")
 
-account_balance = 1000
-while True:
-    print("Choose between the four options below using the options number...")
-    print("1. Withdraw")
-    print("2. Deposit")
-    print("3. Check balance")
-    print("4. Exit")
+# account_balance = 1000
+# while True:
+#     print("Choose between the four options below using the options number...")
+#     print("1. Withdraw")
+#     print("2. Deposit")
+#     print("3. Check balance")
+#     print("4. Exit")
 
-    userChoice=int(input("Your choice: "))
+#     userChoice=int(input("Your choice: "))
 
-    if userChoice == 1:
-        amount=int(input("How much do you want to withdraw? "))
+#     if userChoice == 1:
+#         amount=int(input("How much do you want to withdraw? "))
 
-        if amount <= account_balance:
-            account_balance -= amount
-            print("You have successfully withdrew the amount. ")
-            print("The current balance is: $" + str(account_balance))
-        else:
-            print("Error: Declined. Not enough money!")
+#         if amount <= account_balance:
+#             account_balance -= amount
+#             print("You have successfully withdrew the amount. ")
+#             print("The current balance is: $" + str(account_balance))
+#         else:
+#             print("Error: Declined. Not enough money!")
 
-    if userChoice == 2:
-        amount= int(input("How much do you want to deposit?:")) 
-        balance += amount
+#     if userChoice == 2:
+#         amount= int(input("How much do you want to deposit?:")) 
+#         balance += amount
         
-    if userChoice == 3:
-        print
+#     if userChoice == 3:
+#         print
 
-    if userChoice == 4:
-        break
+#     if userChoice == 4:
+#         break
+
+
 
 
         
