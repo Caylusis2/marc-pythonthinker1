263f6cfafe3838bbf0f96d53853e05a4d538a0c4
Caylusis2 <caylusis2@yahoo.com>
2025-04-19 06:03:59 +0000

    Apr 19, 2025, 6:03 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,75 @@
-print("Hello from lesson 11")
+# print("Hello from lesson 11")
+
+# px=int(input("What is the price of your item?"))
+
+# # if statements
+
+# if px <= 5: 
+#     print("Sounds good!")
+# elif px <= 50:
+#     print("Are you sure you need this?")
+
+# elif px <= 500:
+#     print("Where are you getting this money from?!")
+
+# else:
+#     print("Don't even think about it!")
+
+# rider1= 125
+# rider2= 158
+
+# results= rider1 > 120 and rider2 > 120
+
+# print(results)
+
+# num=int(input("Give me a number"))
+
+# if num % 3 == 0 and num % 7 == 0:
+#     print("The number is divisible by 3 and 7!")
+
+# rider1= 25
+# rider2= 6
+
+# results= rider1 >= 18 or rider2 >= 18
+
+# print(results)
+
+# age=int(input("What is your age?"))
+
+# results= age < 12 or age > 65
+
+# if results:
+#     print("Ticket Price: $15")
+# else:
+#     print("Ticket Price: $20")
+
+# pw=input("What is the password?")
+
+# if not pw == "Python123":
+#     print("Access Denied.")
+
+# burger=input("Do you want a burger?")
+# fries=input("Do you want fries?")
+# drink=input("Do you want a drink?")
+
+# if burger == "yes" and fries == "yes" and not drink == "yes":
+#     print("Won't you get thirsty?")
+
+# un= "John123"
+# pw= "pw123"
+
+# username=input("What is your username?")
+# password=input("What is your password?")
+
+# if username == un and password == pw:
+#     print("Access granted")
+# elif username == un or password == pw:
+#     print("Either username of passowrd is incorrect")
+# else:
+#     print("Access Denied")
+
+
+
+
+
+
--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,86 @@
-print("Hello from lesson 12")
+# print("Hello from lesson 12")
+
+# num=int(input("Give me a number:"))
+
+# if num % 5 == 0 and num % 3 == 0:
+#     print("The number is divisible by 3 and 5!")
+# else:
+#     print("The number is not divisible by 3 and 5")
+
+# visitors = 0 
+# while visitors < 50:
+#     visitors += 1
+#     print("Number of visitors " + str(visitors))
+
+# visitors = 18
+# while visitors < 30:
+#     visitors += 1
+#     print("Number of visitors " + str(visitors))
+
+# visitors = 4
+# while visitors < 25:
+#     visitors += 1
+#     print("Number of visitors " + str(visitors))
+
+# max_visitors = 30
+# visitors = 0
+
+# while True:
+#     addvisitor = input("Add visitor? ")
+#     if addvisitor == "yes":
+#         visitors += 1
+#         print("Number of visitors " + str(visitors))
+#     if visitors == max_visitors:
+#         break
+
+# order = ""
+# skip_comma= True
+
+
+
+# while True:
+#     user_input = input("What do you want to order?")
+    
+#     if user_input == "end":
+#         break
+    
+#     else:
+#         if skip_comma:
+#             order += user_input
+#             skip_comma= False
+#         else:
+#             order += "," + user_input
+
+# print(order)
+            
+# num = 10
+
+# while num > 0:
+#     print(num)
+#     num -= 1
+# else:
+#     print("Happy New Year!")
+
+# num = 10
+
+# while num > 0:
+#     print(num)
+#     num -= 1
+#     if num == 5:
+#         break
+
+# else:
+#     print("Happy New Year!")
+
+
+
+
+    
+
+
+        
+
+
+
+
+    
