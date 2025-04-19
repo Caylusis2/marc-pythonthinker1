fd8e28f130c363202fee6e87357a8ebf94fcecc2
Caylusis2 <caylusis2@yahoo.com>
2025-04-19 06:38:26 +0000

    Apr 19, 2025, 6:38 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -25,7 +25,7 @@ while True:
         balance += amount
         
     if userChoice == 3:
-        print("You currently have:" + str(balance))
+        print("You currently have:" + int(balance))
 
     if userChoice == 4:
         break
