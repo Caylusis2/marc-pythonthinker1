43dfd457273d49a3118d27f9d248181ba16de592
Caylusis2 <caylusis2@yahoo.com>
2025-04-26 06:38:15 +0000

    Apr 26, 2025, 6:38 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -73,7 +73,7 @@ while True:
     if user_input == "end":
         break
     else:
-        user_toppings.append(pizza_toppings[int(userInput)])
+        user_toppings.append(pizza_toppings[int(userInput)-1])
 
 
 
