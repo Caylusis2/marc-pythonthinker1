0c832cb15ad49d1a61a9f337763b990102c11f4f
Caylusis2 <caylusis2@yahoo.com>
2025-05-24 06:31:43 +0000

    May 24, 2025, 6:31 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -5,11 +5,13 @@ import turtle
 window = turtle.Screen()
 t = turtle.Turtle()
 
-def drawShape(length, num_sides):
-    for i in range(num_sides):
-        t.forward(length)
-        t.left(360/ num_sides)
+# def drawShape(length, num_sides):
+#     for i in range(num_sides):
+#         t.forward(length)
+#         t.left(360/ num_sides)
 
-drawShape(100, 4)
+# drawShape(100, 4)
+
+
 
 window.mainloop()
