9e427af4d799c0363f7ff0d7a4700461a4962e61
Caylusis2 <caylusis2@yahoo.com>
2025-05-24 07:01:04 +0000

    May 24, 2025, 7:01 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -25,7 +25,9 @@ def create_blue_ball():
 
 def move_ball(ball, dx, dy):
     ball.setx(ball.xcor() + dx)
-    ball.sety(ball.ycor())
+    ball.sety(ball.ycor() + dy)
+
+    
 
 screenWidth = 300
 screenHeight = 500
